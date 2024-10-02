2f60d2c06b119769c763700e144761754ed4116b
H51M14\Student <Student@gmail.com>
2024-10-02 16:13:34 +0700

    lab 1

--- 302_Huy_Hong_Hoang_Hao_DucHuy/Lab1/UnitTest1.cs
+++ 302_Huy_Hong_Hoang_Hao_DucHuy/Lab1/UnitTest1.cs
@@ -1,14 +1,56 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using MethodLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using MethodLibrary;
 namespace Lab1
 {
     [TestClass]
     public class UnitTest1
     {
+        MethodLibrary.MethodLibrary o = new  MethodLibrary.MethodLibrary();
         [TestMethod]
         public void TestMethod1()
         {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            
+            int exp = 30;
+            Assert.AreEqual(exp, o.Max(a, b, c));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestMethod2()
+        {
+            int a = -10;
+            int b = 20;
+            int c = 30;
+
+            int exp = 30;
+            Assert.AreEqual(exp, o.Max(a, b, c));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestMethod3()
+        {
+           
+            int a = 100;
+            int b = 20;
+            int c = 30;
+
+            int exp = 30;
+            Assert.AreEqual(exp, o.Max(a, b, c));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestMethod4()
+        {
+            String a = "max";
+            int aConvert = int.Parse(a);
+            int b = 20;
+            int c = 30;
+
+            int exp = 30;
+            Assert.AreEqual(exp, o.Max(aConvert, b, c));
         }
     }
 }
