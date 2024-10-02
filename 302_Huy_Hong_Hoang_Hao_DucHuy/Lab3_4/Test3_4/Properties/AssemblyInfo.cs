db7d0a5d390e7a8072120aa72a750fc5a30f31c3
Huy22032002 <ndhuy2203@gmail.com>
2024-10-02 16:40:55 +0700

    'add3_4'

---
+++ 302_Huy_Hong_Hoang_Hao_DucHuy/Lab3_4/Test3_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test3_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test3_4")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b49056d2-bfd7-4117-b280-f507bcd40425")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 302_Huy_Hong_Hoang_Hao_DucHuy/Lab3_4/Test3_4/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MethodLibrary;
+
+namespace Test3_4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+    "|DataDirectory|\\Data3.csv", "Data3#csv",
+DataAccessMethod.Sequential),
+DeploymentItem("Data3.csv"), TestMethod] 
+
+        public void tinhTienDien()
+        {
+            MethodLibrary.MethodLibrary me = new MethodLibrary.MethodLibrary();
+            double kq = me.TinhTienDien(100,-1);
+            double ex = 81620;
+            Assert.AreEqual(ex, kq);
+        }
+        public void Sum()
+        {
+            MethodLibrary.MethodLibrary me = new MethodLibrary.MethodLibrary();
+            long s = -1; ;
+            long kq = me.Sum(0, out s);
+            long exK = 1;
+            Assert.AreEqual(exK, kq);
+            
+        }
+
+    }
+}
