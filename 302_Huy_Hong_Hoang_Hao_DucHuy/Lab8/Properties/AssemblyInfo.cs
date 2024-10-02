220d36305a93554e2c9f2b3debfefb9157127b6c
ashleynguci <hoangkinboy@gmail.com>
2024-10-02 11:15:15 +0700

    add lab 8

---
+++ 302_Huy_Hong_Hoang_Hao_DucHuy/Lab8/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab8")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57a11612-861b-4591-bae0-af74c0fae2fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 302_Huy_Hong_Hoang_Hao_DucHuy/Lab8/UnitTest1.cs
@@ -0,0 +1,142 @@
+﻿using MethodLibrary;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab8
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] arr = { 10, 10, 10 };
+            int left = 1;
+            int right = 2;
+            o.QuickSort(arr, left, right);
+            CollectionAssert.AreEqual(arr, new int[] { 10, 10, 10 });
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] arr = { 3, 2, 1, 4, 10, 8 };
+            int left = 1;
+            int right = 3;
+            System.Console.WriteLine("hello" + right);
+            o.QuickSort(arr, left, right);
+            CollectionAssert.AreEqual(arr, new int[] { 3, 1, 2, 4, 10, 8 });
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] arr = { 1, -1, 0 };
+            int left = 1;
+            int right = 0;
+            o.QuickSort(arr, left, right);
+            CollectionAssert.AreEqual(arr, new int[] { 1, -1, 0 });
+        }
+        // invalid input
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] arr = { 10 };
+            int left = 1;
+            int right = 2;
+            o.QuickSort(arr, left, right);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int[] arr = { 48, 2, 13, 172 }
+;
+            int left = 1;
+            int right = 5;
+            o.QuickSort(arr, left, right);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string hoang = "hoang";
+            int hoangCover = int.Parse(hoang);
+            int[] arr = { 10, hoangCover, 15 }
+;
+            int left = 0;
+            int right = 2;
+            o.QuickSort(arr, left, right);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int[] arr = { }
+;
+            int left = 0;
+            int right = 2;
+            o.QuickSort(arr, left, right);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int[] arr = { 20, 15, 10 }
+;
+            int left = -10;
+            int right = 2;
+            o.QuickSort(arr, left, right);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int[] arr = { 20, 15, 10 }
+;
+            int left = 5;
+            int right = 2;
+            o.QuickSort(arr, left, right);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int[] arr = { 20, 15, 10 };
+            double left = 10.7;
+            int leftConver = int.Parse(left.ToString());
+            System.Console.WriteLine("hello " + leftConver);
+            int right = 2;
+            o.QuickSort(arr, leftConver, right);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            string hoang = "hoang";
+            int hoangCover = int.Parse(hoang);
+            int[] arr = { 20, 15, 10 }
+;
+            int left = int.Parse(hoang);
+            int right = 2;
+            o.QuickSort(arr, left, right);
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            int[] arr = { 20, 15, 10 }
+;
+            int left = 0;
+            int right = -1;
+            o.QuickSort(arr, left, right);
+            //CollectionAssert.AreEqual(arr, new int[] { 20, 15, 10 });
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            int[] arr = { 48, 20 }
+;
+            int left = -1;
+            int right = -1;
+            o.QuickSort(arr, left, right);
+            //CollectionAssert.AreEqual(arr, new int[] { 20, 15, 10 });
+        }
+    }
+    }
